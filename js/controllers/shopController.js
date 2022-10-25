51cd7e48d0690576cb2d33161b1add574e7584ef
zickbalazs <balazs.zick2@gmail.com>
2022-10-25 14:09:18 +0200

    Képek hozzáadva

--- js/controllers/shopController.js
+++ js/controllers/shopController.js
@@ -2,33 +2,38 @@ app.controller('shopController', function($scope){
     $scope.products = [
         {
             itemName:'Talicska',
-            description: 'Egy piros Talicska egy kerékkel.',
+            description: 'Egy talicska négy kerékkel.',
             price: 7800,
-            unit: '/db'
+            unit: '/db',
+            img:'./img/shop/talicska.jpg'
         },
         {
             itemName:'Növényföld',
             description: 'Egy zsák növényeknek való föld.',
             price: 7800,
-            unit: '/10kg'
+            unit: '/10kg',
+            img:'./img/shop/soil.png'
         },
         {
             itemName:'Szárított marhatrágya',
-            description: 'Egy zsák szárított marhatrágya.',
+            description: 'Egy zsák szárított marhatrágya. Vegyél egyet Ginál Zsolt Felebarátunknak!',
             price: 7800,
-            unit: '/db'
+            unit: '/5kg',
+            img:'./img/shop/tragya.jpg'
         },
         {
             itemName:'Stihl© Sövényvágó',
             description: 'A legjobb©©©©© sövényvágó készülék.',
-            price: 17800,
-            unit: '/db'
+            price: 178000,
+            unit: '/db',
+            img:'./img/shop/stihl.png'
         },
         {
             itemName:'Borélesztő',
             description: 'Egy zacskó borélesztő.',
             price: 800,
-            unit: '/db'
+            unit: '/db',
+            img:'./img/shop/boreleszto.jpg'
         }
     ]
 })
